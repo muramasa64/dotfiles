f2701f1e9f71d2fcd6021b8f9c5b8664df83e9c2
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-04-27 12:32:13 +0900

    add vscode settings

--- dotfileslink.rb
+++ dotfileslink.rb
@@ -28,6 +28,24 @@ dotfiles.each do |f|
   end
 end
 
+vscode_jsons = [
+  'settings.json',
+  'keybindings.json'
+]
+
+dir = File.join(File.dirname(File.expand_path(__FILE__)),'vscode')
+vscode_jsons.each do |f|
+  begin
+    src = File.join(dir, f)
+    dst = File.join(ENV['HOME'], 'Library/Application Support/Code/User', f)
+    File.unlink dst if FileTest::symlink? dst
+    File.symlink(src, dst)
+  rescue => e
+    $stderr.puts e
+    exit 1
+  end
+end
+
 # install vim-plug
 `mkdir -pv ~/.vim/autoload/`
 `curl -fLo ~/.vim/autoload/plug.vim  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
